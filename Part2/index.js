43ef107ec71ce34cd4aeb29fdf0964da516316d7
McKay Waters <mckaypaswaters@gmail.com>
2019-08-19 18:57:04 -0600

    finished

---
+++ Part2/index.js
@@ -0,0 +1,9 @@
+let idInput = document.getElementById('idInput')
+let colorInput = document.getElementById('colorInput')
+
+function setCard(){
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+    console.log(card)
+}
+
